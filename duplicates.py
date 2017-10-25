b420aa6f834084a9b9d3943fa98df68c1b022fbe
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 19:35:56 +0500

    поиск файлов в папке и подпапках

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,14 @@
+import os
+
+
+def find_duplicates(parent_directory):
+    # Dups in format {hash:[names]}
+    dups = {}
+    for dirName, subdirs, fileList in os.walk(parent_directory):
+        print('Scanning %s...' % dirName)
+        for filename in fileList:
+            print(filename)
+
 
 if __name__ == '__main__':
-    pass
+    find_duplicates('./')
