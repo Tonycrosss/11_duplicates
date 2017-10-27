2a30997afdd4c64bacbd4f8e6bcd151c6a57d77a
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-27 17:31:47 +0500

    Реализован поиск повторяющихся имен

--- duplicates.py
+++ duplicates.py
@@ -4,10 +4,14 @@ import os
 def find_duplicates(parent_directory):
     # Dups in format {hash:[names]}
     dups = {}
+    filename_list = []
     for dirName, subdirs, fileList in os.walk(parent_directory):
         print('Scanning %s...' % dirName)
         for filename in fileList:
-            print(filename)
+            if filename in filename_list:
+                dups['{}'.format(filename)] = '{}'.format(dirName)
+            else:
+                filename_list.append(filename)
 
 
 if __name__ == '__main__':
