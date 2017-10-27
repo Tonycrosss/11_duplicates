9e58f8b079b0db495a800143684d25e4b06226a8
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-27 17:47:29 +0500

    реализована функция, считающая мд5

--- duplicates.py
+++ duplicates.py
@@ -1,10 +1,23 @@
 import os
+import sys
+import hashlib
+
+
+def hash_counter(path_to_file, blocksize = 65536):
+    with open(path_to_file, 'rb') as file_handler:
+        hasher = hashlib.md5()
+        buffer = file_handler.read(blocksize)
+        while len(buffer) > 0:
+            hasher.update(buffer)
+            buffer = file_handler.read(blocksize)
+            return hasher.hexdigest()
 
 
 def find_duplicates(parent_directory):
     # Dups in format {hash:[names]}
     dups = {}
     filename_list = []
+    # TODO: Нормальные имена переменных
     for dirName, subdirs, fileList in os.walk(parent_directory):
         print('Scanning %s...' % dirName)
         for filename in fileList:
@@ -13,6 +26,7 @@ def find_duplicates(parent_directory):
             else:
                 filename_list.append(filename)
 
-
 if __name__ == '__main__':
     find_duplicates('./')
+    print(hash_counter('./dubles/alala.txt'))
+    print(hash_counter('./dubles/dubles1/alala.txt'))
